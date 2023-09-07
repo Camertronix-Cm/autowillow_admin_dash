b2233cee148539c0904ac82b9a63a0766b3f4d50
gwagsi <gwagsiglenn@gmail.com>
2023-09-07 09:13:39 +0100

    message box

--- components/chats/index.tsx
+++ components/chats/index.tsx
@@ -1,7 +1,9 @@
-
-import {Button, Input, Text} from '@nextui-org/react';
+import { Button, Input, Text } from "@nextui-org/react";
+import Header from "./Header";
+import { Avatar } from "@nextui-org/react";
+import Form from "./messageInputSection";
+import MessageBox from "./MessageBox";
 
 export const Chats = () => {
-    return <Text h3>All Accounts</Text>
-}
-
+  return <MessageBox/>
+};
